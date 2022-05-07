409b7f049b3d32529d06e2afd26e9b3172698fbe
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-05-07 12:58:55 +0500

    create new template,action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root  'pages#home'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  # this line will give a new article path which can be used to show delete update or create new article path
 end
